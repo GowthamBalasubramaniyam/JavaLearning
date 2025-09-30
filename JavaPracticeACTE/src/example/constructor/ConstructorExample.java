832e8710548560d369bf264ae1762bc21c7f7d70
GowthamBalasubramaniyam <gowthambalasubramaniyam1@gmail.com>
2025-09-30 18:59:19 +0530

    sample commit

--- JavaPracticeACTE/src/example/constructor/ConstructorExample.java
+++ JavaPracticeACTE/src/example/constructor/ConstructorExample.java
@@ -4,16 +4,23 @@ public class ConstructorExample {
 	int a = 10;
 	int b = 10;
 
+	//constructor without paramenters
+	ConstructorExample() {
+		int c = a + b;
+		System.out.println(c);
+	}
 	
-
+	//constructor with paramenters
 	ConstructorExample(int ad, int bc) {
 		a = ad;
 		b = bc;
 		System.out.println(a * b);
 	}
 	
-	ConstructorExample() {
-		int c = a + b;
-		System.out.println(c);
+	ConstructorExample(int a , int b, String c){
+		this.a = a;
+		this.b = b;
+		
 	}
+	
 }
